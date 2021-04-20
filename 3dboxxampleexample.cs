33a4047d25d9c568efd9e7acbc7a54e2930c9b19
Justin Staines <justin.staines@shumbi.com>
2021-04-20 08:19:50 +0100

    Add files via upload

---
+++ 3dboxxampleexample.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace SDKSample
+{
+    public partial class Basic3DShapeExample : Page
+    {
+        public Basic3DShapeExample()
+        {
+
+           
+        }
+    }
+}
